8c81e6b0304f9ee2d9cbf0b7349d6aaa88c7f185
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-11-29 17:04:47 +0700

    Update getRequest.php

--- api/getRequest.php
+++ api/getRequest.php
@@ -21,7 +21,7 @@ if(iseet($_POST['userid']))
 
 $dbconn = pg_connect("host=ec2-54-227-241-179.compute-1.amazonaws.com
   port=5432 dbname=d4ieg9ce7qihnf user=doirzncaoefasd password=0955cadb61b87148265f253f9b11a740c24b806bbb7d9b24c2b992da74861a99");
-  $sql = "SELECT * FROM \"public\".\"request_friends\" WHERE to_userid='$userid' ";
+  $sql = "SELECT * FROM \"public\".\"request_friends\" ";
 //$data = $dbconnection->select($sql);
 $data=pg_query($dbconn,$sql);
 
