fa083854e00d81f6011131c225f0c59797846f16
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-11-29 17:15:13 +0700

    Update getRequest.php

--- api/getRequest.php
+++ api/getRequest.php
@@ -14,7 +14,7 @@ $res=null;
           
       }
  }
-if(isset($_POST["userid"])) {
+
   $userid=$_POST["userid"];
   $dbconn = pg_connect("host=ec2-54-227-241-179.compute-1.amazonaws.com
    port=5432 dbname=d4ieg9ce7qihnf user=doirzncaoefasd password=0955cadb61b87148265f253f9b11a740c24b806bbb7d9b24c2b992da74861a99");
@@ -31,6 +31,10 @@ $data=pg_query($dbconn,$sql);
      }      
     // Chuyen dinh dang cua mang thanh JSON
      echo json_encode($array);
+
+
+
+
 
 
 
