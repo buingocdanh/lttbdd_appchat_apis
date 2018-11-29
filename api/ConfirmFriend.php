fa1e0df7fd06bc22abde568c10baa4c09c7a335a
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-11-29 23:03:57 +0700

    Update ConfirmFriend.php

--- api/ConfirmFriend.php
+++ api/ConfirmFriend.php
@@ -14,7 +14,7 @@ if(isset($_POST['user_id'])&&isset($_POST['friend_id'])){
   $dbconnection = new postgresql("");
 
   $sql_send_request = "INSERT INTO public.friend(user_id,friend_id,note)
-        VALUES ('$user_id','$friend_id')";
+        VALUES ('$user_id','$friend_id','not')";
   $dbconnection->execute($sql_send_request);
 
   $res = new Result(0,'Send request successfully');
