2f033f2977bb5e65530e1b5422f9dec490b1b8df
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-11-29 15:47:37 +0700

    aaa
    aaaa

---
+++ api/request_friends.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-Type: application/json');
+include "../lib/data.php";
+//Lấy data từ client
+$res = null;
+if(isset($_POST['to_userid'])&&isset($_POST['from_userid'])){
+  $to_userid = $_POST['to_userid'];
+  $from_userid = $_POST['to_userid'];
+
+  //Kết nối database
+  include ('../lib/db.php');
+
+
+  $dbconnection = new postgresql("");
+  $sql_send_request = "INSERT INTO public.user(to_userid,from_userid,send_date,note)
+        values ('$to_userid','$from_userid',CURRENT_DATE,'hello')";
+  $dbconnection->execute($sql_send_request);
+
+  $res = new Result(Constant::SUCCESS,'Send request successfully');
+      
+  $dbconnection->close();
+
+echo (json_encode($res));
+?>
