39782d1c45ecf40b08066c9b6b574af728c36284
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-11-29 17:09:01 +0700

    Update getRequest.php

--- api/getRequest.php
+++ api/getRequest.php
@@ -15,13 +15,11 @@ $res=null;
       }
  }
 
-if(iseet($_POST['userid']))
-{
- $userid=$_POST['userid']);
+
 
 $dbconn = pg_connect("host=ec2-54-227-241-179.compute-1.amazonaws.com
   port=5432 dbname=d4ieg9ce7qihnf user=doirzncaoefasd password=0955cadb61b87148265f253f9b11a740c24b806bbb7d9b24c2b992da74861a99");
-  $sql = "SELECT * FROM \"public\".\"request_friends\" ";
+  $sql = "SELECT * FROM \"public\".\"request_friends\" WHERE to_userid='74'";
 //$data = $dbconnection->select($sql);
 $data=pg_query($dbconn,$sql);
 
@@ -32,12 +30,9 @@ $data=pg_query($dbconn,$sql);
      {
         array_push($array, new RequestFriend($row['to_userid'],$row['from_userid'],$row['send_date'],$row['note']));
      }      
- 
-    
     // Chuyen dinh dang cua mang thanh JSON
      echo json_encode($array);
 
-}
 
 
 
