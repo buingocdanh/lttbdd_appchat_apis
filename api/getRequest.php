84a66d109ad23fbee7f2ca5792744cae3be9861a
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-11-29 16:20:38 +0700

    a
    aaaaa

---
+++ api/getRequest.php
@@ -0,0 +1,62 @@
+<?php
+header('Content-Type: application/json');
+include "../lib/data.php";
+$res=null;
+/**
+ * 
+ */
+ class RequestFriend{
+      function RequestFriend($to_userid,$from_userid,$send_date,$note){
+            $this->to_userid=$to_userid;
+            $this->from_userid=$from_userid;
+            $this->send_date=$send_date;
+            $this->note=$note;
+          
+      }
+ }
+if(isset($_POST["userid"])){
+
+    $from_userid = $_POST["userid"];
+    $sql = "SELECT * FROM \"public\".\"request_friends\" WHERE to_userid='userid'";
+     // ket noi database
+     include "../lib/db.php";
+     //$dbconnection=getDatabase();
+     $dbconnection = new postgresql("");
+     $result = $dbconnection->select($sql);
+    if($result!==null){
+            $array=array();
+         
+          //add element to arrUser
+            while ($row=mysqli_fetch_assoc($result)) 
+                 {
+                    
+                     array_push($array, 
+                                new RequestFriend(
+                                            $row['to_userid'],
+                                            $row['from_userid'],
+                                            $row['send_date'],
+                                            $row['note'])
+                              );
+                 }
+                      
+    // Chuyen dinh dang cua mang thanh JSON
+           echo json_encode($array);
+
+        }
+      else{
+        //error -1
+          $res = new Result(Constant::GENERAL_ERROR,'Send request not excute');
+          echo (json_encode($res));
+      }  
+    $dbconnection->close();
+
+}
+
+else{
+  //error -1
+        $res = new Result(Constant::GENERAL_ERROR,'Send request not excute');
+        echo (json_encode($res));
+}
+
+
+?>
