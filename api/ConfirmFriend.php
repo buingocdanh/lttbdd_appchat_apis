7c69e6ba999cd31d5a79367a3d4ed6d1a07f5c99
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-11-29 22:56:56 +0700

    aaaaa
    aaaaa

---
+++ api/ConfirmFriend.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-Type: application/json');
+include "../lib/data.php";
+//Lấy data từ client
+$res = null;
+if(isset($_POST['user_id'])&&isset($_POST['friend_id'])){
+  $user_id = $_POST['userid'];
+  $friend_id = $_POST['friend_id'];
+
+  //Kết nối database
+  include ('../lib/db.php');
+
+
+  $dbconnection = new postgresql("");
+
+  $sql_send_request = "INSERT INTO public.friend(user_id,friend_id,note)
+        VALUES ('$user_id','$friedn_id','note')";
+  $dbconnection->execute($sql_send_request);
+
+  $res = new Result(0,'Send request successfully');
+      
+  $dbconnection->close();
+}
+
+  echo (json_encode($res));
+?>
