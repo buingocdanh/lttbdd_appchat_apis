e1ebca7a8e6ea07ac959f7fe74600b3813ee1e79
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-11-29 16:23:08 +0700

    Update getRequest.php

--- api/getRequest.php
+++ api/getRequest.php
@@ -16,8 +16,8 @@ $res=null;
  }
 if(isset($_POST["userid"])){
 
-    $from_userid = $_POST["userid"];
-    $sql = "SELECT * FROM \"public\".\"request_friends\" WHERE to_userid='userid'";
+    $userid = $_POST["userid"];
+    $sql = "SELECT * FROM \"public\".\"request_friends\" WHERE to_userid='$userid'";
      // ket noi database
      include "../lib/db.php";
      //$dbconnection=getDatabase();
