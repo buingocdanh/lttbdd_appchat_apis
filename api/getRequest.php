2387f6ca88023c213577d600a09be4bcc4480046
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-11-29 16:55:30 +0700

    Update getRequest.php

--- api/getRequest.php
+++ api/getRequest.php
@@ -14,53 +14,32 @@ $res=null;
           
       }
  }
-if(isset($_POST["userid"])){
-    echo "xxxxxx";
-    $userid = $_POST["userid"];
-      $sql = "SELECT * FROM \"public\".\"user\" ";
-     // ket noi database
-     include "../lib/db.php";
-     //$dbconnection=getDatabase();
-     $dbconnection = new postgresql("");
-     $result = $dbconnection->select($sql);
-     $so=postgresql_num_rows($result);
-   echo $so;
-    if(postgresql_num_rows($result)>0){
-            $array=array();
-           echo "sssss";
-          //add element to arrUser
-          /*  while ($row=mysqli_fetch_assoc($result)) 
-                 {
-                    
-                     array_push($array, 
-                                new RequestFriend(
-                                            $row['to_userid'],
-                                            $row['from_userid'],
-                                            $row['send_date'],
-                                            $row['note'])
-                              );
-                 }
-                      */
+
+
+
+$dbconn = pg_connect("host=ec2-54-227-241-179.compute-1.amazonaws.com
+  port=5432 dbname=d4ieg9ce7qihnf user=doirzncaoefasd password=0955cadb61b87148265f253f9b11a740c24b806bbb7d9b24c2b992da74861a99");
+  $sql = "SELECT * FROM \"public\".\"user\" ";
+//$data = $dbconnection->select($sql);
+$data=pg_query($dbconn,$sql);
+class User{
+      function User($user_id,$user_name,$full_name){
+            $this->User_ID=$user_id;
+            $this->User_Name=$user_name;
+            $this->Full_Name=$full_name;
+      }
+ }
+   // create array user
+     $arrUser=array();
+   //add element to arrUser
+     while ($row=pg_fetch_array($data)) 
+     {
+        array_push($arrUser, new User($row['user_id'],$row['user_name'],$row['full_name']));
+     }      
     // Chuyen dinh dang cua mang thanh JSON
-           echo json_encode($array);
+     echo json_encode($arrUser);
 
-        }
-      else{
-        echo "kjkkk";
-        //error -1
-          $res = new Result(Constant::GENERAL_ERROR,'Send request not excute');
-          echo (json_encode($res));
-      }  
-    $dbconnection->close();
 
-}
-
-else{
-       echo "loi post";
-  //error -1
-        $res = new Result(Constant::GENERAL_ERROR,'Send request not excute');
-        echo (json_encode($res));
-}
 
 
 ?>
