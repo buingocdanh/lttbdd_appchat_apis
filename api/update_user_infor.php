1ed399014d8695d90a0b1b0a1e490737cb9c9632
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-12-01 02:00:58 +0700

    Update update_user_infor.php

--- api/update_user_infor.php
+++ api/update_user_infor.php
@@ -3,22 +3,41 @@ header('Content-Type: application/json');
 include "../lib/data.php";
 
 $res = null;
-if (isset($_POST["username"]) && isset($_POST["fullname"]) && isset($_POST["email"]) && isset($_POST["gender"]) && isset($_POST["password"]) )  {
+if (isset($_POST["username"]) 
+   && isset($_POST["fullname"]) 
+   && isset($_POST["email"]) 
+   && isset($_POST["gender"]) 
+   && isset($_POST["password"]) ) 
+{
   $username = $_POST["username"];
   $fullname = $_POST["fullname"];
   $email = $_POST["email"];
   $gender = $_POST["gender"];
-  $password = $_POST["password"];
-  //Có cần xác nhận mật khẩu cũ mới cho cập nhật không?
-  $sql = "UPDATE \"public\".\"user\" SET full_name='$username',email = '$email', pass_word = '$password',gender = 'gender' WHERE user_name = '$username'";
+  $password = hash('sha256',$_POST["password"]);
+ 
+  $sql = "UPDATE \"public\".\"user\" SET 
+      full_name='$fullname',
+	  email = '$email', 
+	  pass_word = '$password',
+	  gender = 'gender' 
+	  WHERE user_name = '$username'";
   
-  $dbconnection = new postgresql("");
+ // ket noi database
+    include "../lib/db.php";
+    //$dbconnection=getDatabase();
+   $dbconnection = new postgresql("");
   
-  $dbconnection->execute($sql);
-  
-  $res = new Result(Constant::SUCCESS,'Update user information successfully');
-  
+  $result= $dbconnection->execute($sql);
+  if($result!==null)
+  {
+	   $res = new Result(Constant::SUCCESS,'Update user information successfully');
+  }
+  else{
+	    $res=new Result(-1,"Request not excute");
+	  }
+ 
 } else {
     $res = new Result(Constant::INVALID_PARAMETERS, 'Invalid parameters.');
 }
-echo (json_encode($res));}
+echo (json_encode($res));
+?>
